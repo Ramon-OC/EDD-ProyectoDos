4f1a06ee34a0e8fb17c1e80c452b2e4115ca08d4
EmiMau <106214079+EmiMau@users.noreply.github.com>
2022-06-01 18:59:02 -0500

    Update Frecuencia.java

--- Frecuencia.java
+++ Frecuencia.java
@@ -1,6 +1,6 @@
 /**
 * Crea objetos frecuencia, se encargan de registrar la cantidad de alumnos registrados por edad
-* @author // Inserta tu nombre completo y número de cuenta
+* @author Chávez Zamora Mauro Emiliano - 111001079
 * @author Ortiz Castañeda José Ramón - 318357115
 * @version 1.0 Mayo 2022
 * @since Estructuras de Datos 2022-1.
